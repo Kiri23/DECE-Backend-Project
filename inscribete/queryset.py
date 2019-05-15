27ab141a2ca6d5f78c1b08f25be48969c1672a7c
Christian Nogueras <>
2019-05-15 16:06:14 -0400

    Matricula now has some layout

--- inscribete/queryset.py
+++ inscribete/queryset.py
@@ -5,3 +5,6 @@ class InscribeteQueryset(models.QuerySet):
 
     def porEstudiante(self, estudianteId):
         return self.filter(estudiante__pk=estudianteId)
+
+    def costoTotalPorEstudiante(self, estudianteId):
+        pass
